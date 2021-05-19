b6e94c2b4e201203d4d81945f62df35435902a0a
Kouki Aishima <miki.0418.kouki@gmail.com>
2021-05-19 23:52:50 +0900

    更新

--- chap1_2.php
+++ chap1_2.php
@@ -5,4 +5,4 @@ var_dump("下底は？");
 $bottom = trim(fgets(STDIN));
 var_dump("高さは？");
 $height = trim(fgets(STDIN));
-var_dump("面積は".($top + $bottom) * $height / 2);
+var_dump("面積は" . ($top + $bottom) * $height / 2);
