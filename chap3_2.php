52673ef612dcae6440ebad1caf679c1de39e0f91
Kouki Aishima <miki.0418.kouki@gmail.com>
2021-06-01 22:48:07 +0900

    課題2.3修正

--- chap3_2.php
+++ chap3_2.php
@@ -1,8 +1,8 @@
 <?
 foreach (range(1, 100) as $int) {
     if ($int % 3 == 0) {
-        var_dump($int = "アホ");
+        echo($int = "アホ");
     } else {
-        var_dump($int);
+        echo($int);
     }
 }
--- chap3_3.php
+++ chap3_3.php
@@ -1,12 +1,12 @@
 <?
 foreach (range(1, 100) as $int) {
-    if ($int % 3 == 0) {
-        var_dump($int = "Fizz");
-    } elseif ($int % 5 == 0) {
-        var_dump($int = "Buzz");
-    } elseif ($int % 3 == 0 && $int % 5 == 0) {
-        var_dump($int = "FizzBuzz");
+    if ($int % 3 == 0 && $int % 5 == 0) {
+        echo ($int = "FizzBuzz");
+    } elseif($int % 5 == 0) {
+        echo ($int = "Buzz");
+    } elseif($int % 3 == 0) {
+        echo( $int = "Fuzz");
     } else {
-        var_dump($int);
+        echo ($int);
     }
 }
