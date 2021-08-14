6d12c1ebb3ad963f21886186bc006399b3a76a35
Kouki Aishima <miki.0418.kouki@gmail.com>
2021-08-14 18:46:06 +0900

    行き詰まり

--- chap4_1.php
+++ chap4_1.php
@@ -1,4 +1,15 @@
 <?php
 require_once __DIR__ . "/chap4_area.php";
-displayMsg(10, 20, 30);
-displayMsg(40, 50, 60);
+$top = 10;
+$bottom = 20;
+$height = 30;
+
+$area = calcArea($top, $bottom, $height);
+displayMsg($top, $bottom, $height, $area);
+
+$top = 40;
+$bottom = 50;
+$height = 60;
+
+$area = calcArea($top, $bottom, $height);
+displayMsg($top, $bottom, $height, $area);
---
+++ chap4_price.php
@@ -0,0 +1,16 @@
+<?php
+function displayMsg( $name, $sum, $price ){
+    $msg = <<< EOM
+
+{$name}様
+ご注文を承りました。
+合計{$sum}円です。
+{$price}円(税込)になります。\n
+
+EOM;
+    echo $msg;
+}
+
+function add( $item1, $item2, $item3 ){
+    return $sum =  $item1 + $item2 + $item3; 
+}
