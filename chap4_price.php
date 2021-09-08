d352582d9f4e8ad744a5969682a3dce7276cc5cd
Kouki Aishima <miki.0418.kouki@gmail.com>
2021-09-08 22:08:11 +0900

    変更

--- chap4_price.php
+++ chap4_price.php
@@ -1,16 +1,29 @@
 <?php
+var_dump("お名前は？");
+$name = trim(fgets(STDIN));
+
+var_dump("1つ目の金額を入力");
+$item1 = trim(fgets(STDIN));
+
+var_dump("2つ目の金額を入力");
+$item2 = trim(fgets(STDIN));
+
+var_dump("3つ目の金額を入力");
+$item3 = trim(fgets(STDIN));
+
 function displayMsg( $name, $sum, $price ) {
     $msg = <<<EOM
 
 {$name}様
 ご注文を承りました。
-合計{$sum}円です。
-{$price}円(税込)になります。\n
+合計{$price}円です。
+{$sum}円(税込)になります。\n
 
 EOM;
     echo $msg;
 }
 
 function add( $item1, $item2, $item3 ) {
-    return $sum =  $item1 + $item2 + $item3; 
+    return $sum =  floor($item1 + $item2 + $item3); 
 }
+
